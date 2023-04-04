8493057ed7b4fd466091b8c92ad500430557a68e
nguyenhoanganu <129148984+nguyenhoanganu@users.noreply.github.com>
2023-04-04 16:36:08 +0700

    Update CTDL&GT_Insertion_Sort_1.cpp

--- CTDL&GT_Insertion_Sort_1.cpp
+++ CTDL&GT_Insertion_Sort_1.cpp
@@ -1,12 +1,15 @@
-void Insertion_Sort(int a[], int n){
+void Insertion_Sort(int a[], int n)
+{
 	int pos, i;
-	int x;//lưu giá trị a[i] tránh bị ghi đè khi dời chỗ các phần tử
-	for(i=1; i<n; i++){//đoạn a[0] đã sắp xếp
+	int x;
+	for(i=1; i<n; i++){
 		x = a[i]; pos = i-1;
 		//tìm vị trí chèn x
-		while((pos>=0)&&(a[pos]>x)){
-                //kết hợp dời chỗ các phần tử sẽ đứng sau x trong danh sách mới
+		while((pos>=0)&&(a[pos]>x))
+		{
 			a[pos+1] = a[pos];
 			pos--;
 		}
-		a[pos+1] = x;//chèn x vào danh sách
+		a[pos+1] = x;
+	}
+}
